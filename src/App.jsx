5db83578b0a788c4897ef2574093e8c37a19e088
Sudesh Kalokhe <sudeshkalokhe3103@gmail.com>
2026-04-06 16:52:57 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -54,6 +54,7 @@ const AppContent = () => {
     navigate: navigateTo,
   };
 
+  
   return (
     <div className="flex flex-col min-h-screen bg-white">
       <main className="flex-1 flex flex-col">
